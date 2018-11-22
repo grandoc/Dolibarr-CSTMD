ac8a740af9d21ab9918450fa6cac59bc06364db1
Philippe GRAND <philippe.grand@atoo-net.com>
2018-11-22 23:13:42 +0100

    clean and update code

--- icstmd/attestConsSec.php
+++ icstmd/attestConsSec.php
@@ -1,5 +1,20 @@
 <?php
-/* <one line to give the program's name and a brief idea of what it does.> * Copyright (C) <2017> SaaSprov.ma <saasprov@gmail.com> * * This program is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. * * This program is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) <2017> SaaSprov.ma <saasprov@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // Load Dolibarr environment
 if (false === (@include '../main.inc.php')) {  // From htdocs directory
 	require '../../main.inc.php'; // From "custom" directory
@@ -64,7 +79,7 @@ class PDF extends FPDF
 }
 // Instanciation de la classe dérivée
 $pdf = new PDF(array($conf->global->FOOTER_LIGNE1, $conf->global->FOOTER_LIGNE2, $conf->global->FOOTER_LIGNE3, $conf->global->FOOTER_LIGNE4));
-$pdf->AliasNbPages();
+if (method_exists($pdf,'AliasNbPages')) $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 
